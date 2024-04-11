8dad38349455771d813f51792efed0b9ef199b06
SUPREME-CONDEMNER <164725996+SUPREME-CONDEMNER@users.noreply.github.com>
2024-04-11 20:30:02 +0500

    Update xmr.sh

--- xmr.sh
+++ xmr.sh
@@ -11,36 +11,34 @@ DONATION_LEVEL=0
 
 # Installation Process
 clear
-echo -e "\e[93m               Installation Started               "
+echo -e "\e[93mInstallation Started\e[0m"
 sleep 3
 
 # Install Libraries
 clear
-echo -e "\e[93m               Importing Libraries                "
-add-apt-repository -y ppa:jonathonf/gcc-7.1 > /dev/null 2>&1
-apt-get update > /dev/null 2>&1
-apt-get install -y libmicrohttpd-dev > /dev/null 2>&1
+echo -e "\e[93mImporting Libraries\e[0m"
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install -y libmicrohttpd-dev > /dev/null 2>&1
 
 # Fetching Latest XMRig Version
 clear
-echo -e "\e[93m              Fetching Latest XMRig               "
+echo -e "\e[93mFetching Latest XMRig\e[0m"
 latest_version=$(curl -s "https://api.github.com/repos/xmrig/xmrig/releases/latest" | grep -oP '"tag_name": "\K(.*)(?=")')
 download_url="https://github.com/xmrig/xmrig/releases/download/${latest_version}/xmrig-${latest_version}-linux-x64.tar.gz"
 
 # Install XMRIG Binary
 clear
-echo -e "\e[93m             Installing XMRIG Binary              "
-cd /tmp
-wget --quiet $download_url
-tar -xf xmrig-${latest_version}-linux-x64.tar.gz
-mv xmrig-${latest_version}/xmrig /usr/local/bin/xmrig
-chmod +x /usr/local/bin/xmrig
+echo -e "\e[93mInstalling XMRIG Binary\e[0m"
+sudo mkdir -p /usr/local/bin/xmrig
+cd /usr/local/bin/xmrig || exit
+sudo wget --quiet "$download_url"
+sudo tar -xf "xmrig-${latest_version}-linux-x64.tar.gz" --strip-components=1
+sudo chmod +x xmrig
 
 # Setting Up Config
 clear
-echo -e "\e[93m                Setting Up Config                 "
-cd /etc
-cat <<EOF > config.json
+echo -e "\e[93mSetting Up Config\e[0m"
+cat <<EOF | sudo tee /etc/xmrig.conf > /dev/null
 {
     "algo": "${ALGO}",
     "url": "${POOL_URL_AND_PORT}",
@@ -57,5 +55,5 @@ EOF
 
 # Starting Miner
 clear
-echo -e "\e[93m                 Starting Miner                   "
-xmrig -c /etc/config.json
+echo -e "\e[93mStarting Miner\e[0m"
+sudo xmrig -c /etc/xmrig.conf

